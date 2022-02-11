f47cb65549fb08f6ed2f1a4aa1e040c58353be8a
root <root@ip-172-31-38-79.ap-south-1.compute.internal>
2022-02-11 19:26:14 +0000

    initial infra

--- main.tf
+++ main.tf
@@ -29,6 +29,15 @@ resource "aws_security_group" "webserver" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+ingress {
+    description      = ""
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 	
   egress {
     from_port        = 0
